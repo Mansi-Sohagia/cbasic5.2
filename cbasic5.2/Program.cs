68cc53c77dc55e3b0557926abc3e22336caf43c5
TechJerryyy <msohagia@aakashinformatics.com>
2023-02-15 16:59:01 +0530

    Add project files.

---
+++ cbasic5.2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+ */
+
+using System;
+
+namespace BasicCSharpExcercise5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SUM = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter Number: ");
+                var n = Console.ReadLine();
+                if (n == "ok")
+                    break;
+                var numbers = Convert.ToInt32(n);
+                SUM += numbers;
+            }
+            Console.WriteLine("sum= " + SUM);
+        }
+    }
+}
